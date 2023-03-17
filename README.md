d87e3b107c16419f15fd937330fc749f0a8ca956
andy <aphanIT@protonmail.com>
2023-03-16 23:52:10 -0400

    setting up Firebase locally

--- README.md
+++ README.md
@@ -2,16 +2,16 @@
 Repo for Fuchsia Cuttlefish Team!
 
 
-TECHSTACK\
-Backend - [Firebase](https://www.freecodecamp.org/news/building-a-real-time-chat-app-with-reactjs-and-firebase/) - I followed this guide\
-Frontend - undecided
+## TECHSTACK\
+Backend - Firebase [guide](https://www.freecodecamp.org/news/building-a-real-time-chat-app-with-reactjs-and-firebase/)\
+Frontend - React
 
 
-PREREQUISUITES\
+## PREREQUISUITES\
 [NodeJS](https://nodejs.org/en)
 
 
-[FIREBASE LOCAL SET UP](https://firebase.google.com/docs/emulator-suite`) - I followed this guide\
+## FIREBASE LOCAL SET UP - [guide](https://firebase.google.com/docs/emulator-suite)\
 Run `npm install firebase`\
 I had to use `npm install -g firebase-tools -f` to rectify `'firebase' is not recognized as an internal or external command'` error\
 Navigate to your JavaScript Project and run `firebase init`\
---
+++ src/components/ChatBox.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef, useState } from "react";
+import {
+  query,
+  collection,
+  orderBy,
+  onSnapshot,
+  limit,
+  QuerySnapshot,
+  Query,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import Message from "./Message";
+import SendMessage from "./SendMessage";
+
+const ChatBox = () => {
+  const [messages, setMessages] = useState([]);
+  const scroll = useRef();
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "messages"),
+      orderBy("createdAt"),
+      limit(50)
+    );
+
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      let messages = [];
+      QuerySnapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
+    });
+    return () => unsubscribe;
+  }, []);
+
+  return (
+    <main className="chat-box">
+      <div className="messages-wrapper">
+        {messages?.map((message) => (
+          <Message key={message.id} message={message} />
+        ))}
+      </div>
+      <span ref={scroll}></span>
+      <SendMessage />
+    </main>
+  );
+};
+
+export default ChatBox;
