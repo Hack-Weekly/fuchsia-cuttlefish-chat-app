7c3aab1c9811e38b1deb8081acc3f470bcff35c7
Andy <aphanit@protonmail.com>
2023-03-24 01:56:07 -0400

    Revert "Add splash screen"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import './App.css';
-import NavBar from './components/NavBar';
-import ChatContainer from './components/ChatContainer';
-import Welcome from './components/Welcome';
-import { auth } from './firebase';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import React, { useState, useEffect } from 'react';
-import SplashScreen from './components/SplashScreen';
+import "./App.css";
+import NavBar from "./components/NavBar";
+import ChatContainer from "./components/ChatContainer";
+import Welcome from "./components/Welcome";
+import { auth } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 500);
-  }, []);
-
   const [user] = useAuthState(auth);
   return (
-    <div className='App'>
-      {loading ? <SplashScreen /> : <></>}
+    <div className="App">
       <NavBar />
       {!user ? <Welcome /> : <ChatContainer />}
     </div>
