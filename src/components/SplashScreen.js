6cc9fe2cf10a4fd3ebf3b2b978d84df63d19cde9
andy <aphanIT@protonmail.com>
2023-03-24 01:47:42 -0400

    Add splash screen

---
+++ src/components/SplashScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SplashScreen = () => {
+  return (
+    <div className='splash-screen'>
+      <img
+        src='/logo192.png'
+        alt='Cuttlefish Club logo'
+        width={250}
+        height={250}
+        className='loading-circle'
+      />
+    </div>
+  );
+};
+
+export default SplashScreen;
--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,31 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAxkbHe9aKcO2iiD4yqfDNme4u7LvnLaOg",
+  authDomain: "fuchsia-cuttlefish-chat-b77e2.firebaseapp.com",
+  databaseURL: "https://fuchsia-cuttlefish-chat-b77e2-default-rtdb.firebaseio.com",
+  projectId: "fuchsia-cuttlefish-chat-b77e2",
+  storageBucket: "fuchsia-cuttlefish-chat-b77e2.appspot.com",
+  messagingSenderId: "520032598993",
+  appId: "1:520032598993:web:c1e321872ebd2fd3f17ef0",
+  measurementId: "G-DW0CBL4Y1V"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+
+let db, auth;
+const emulator = true;
+if (emulator) {
+  auth = getAuth(app);
+  connectAuthEmulator(auth, "http://localhost:9099");
+
+  db = getFirestore(app);
+  connectFirestoreEmulator(db, "localhost", 8080);
+} else {
+  auth = getAuth()
+  db = getFirestore()
+}
+
+export { auth, db };
