cec24abed9db10859ba8cc4fe6eb6ef95dc71ede
andy <aphanIT@protonmail.com>
2023-03-24 01:58:46 -0400

    splash screen with proper firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,31 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, connectAuthEmulator } from "firebase/auth";
-import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAxkbHe9aKcO2iiD4yqfDNme4u7LvnLaOg",
-  authDomain: "fuchsia-cuttlefish-chat-b77e2.firebaseapp.com",
-  databaseURL: "https://fuchsia-cuttlefish-chat-b77e2-default-rtdb.firebaseio.com",
-  projectId: "fuchsia-cuttlefish-chat-b77e2",
-  storageBucket: "fuchsia-cuttlefish-chat-b77e2.appspot.com",
-  messagingSenderId: "520032598993",
-  appId: "1:520032598993:web:c1e321872ebd2fd3f17ef0",
-  measurementId: "G-DW0CBL4Y1V"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-
-let db, auth;
-const emulator = true;
-if (emulator) {
-  auth = getAuth(app);
-  connectAuthEmulator(auth, "http://localhost:9099");
-
-  db = getFirestore(app);
-  connectFirestoreEmulator(db, "localhost", 8080);
-} else {
-  auth = getAuth()
-  db = getFirestore()
-}
-
-export { auth, db };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
