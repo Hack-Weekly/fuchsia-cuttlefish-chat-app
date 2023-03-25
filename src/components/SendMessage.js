6b4d62892fd57927b95680fd1cad6efcba04869b
andy <aphanIT@protonmail.com>
2023-03-25 03:10:20 -0400

    eslint errors fixed

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -1,6 +1,6 @@
 import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
 import { MdAddPhotoAlternate as Photo } from 'react-icons/md';
 import { auth, db } from '../firebase';
 
@@ -41,44 +41,37 @@ const SendMessage = ({ scroll, room }) => {
     });
 
     scroll.current?.scrollIntoView({ behavior: 'smooth' });
-    setProgress(0)
+    setProgress(0);
   };
 
-  const uploadImage = file => {
-    // TODO: filter based on list of approved file types
-    const metadata = {
-      contentType: file.type,
-    };
-    const storage = getStorage();
-    const storageRef = ref(storage, 'images/' + file.name);
-    const uploadTask = uploadBytesResumable(storageRef, file, metadata);
-    uploadTask.on(
-      'state_changed',
-      snapshot => {
-        //observe state
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        setProgress(progress);
-        console.log(progress);
-        switch (snapshot.state) {
-          case 'paused':
-            console.log('Upload is paused');
-            break;
-          case 'running':
-            console.log('Upload is running');
-            break;
+  const uploadImage = useCallback(
+    file => {
+      // TODO: filter based on list of approved file types
+      const metadata = {
+        contentType: file.type,
+      };
+      const storage = getStorage();
+      const storageRef = ref(storage, 'images/' + file.name);
+      const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+      uploadTask.on(
+        'state_changed',
+        snapshot => {
+          //observe state
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setProgress(progress);
+        },
+        error => {
+          // handle error
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(downloadURL => setImageUrl(downloadURL));
         }
-      },
-      error => {
-        // handle error
-      },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then(downloadURL => setImageUrl(downloadURL));
-      }
-    );
-  };
+      );
+    },
+    [setProgress, setImageUrl]
+  );
 
   const imgInputStyle = {
-    borderRadius: `0`,
     marginRight: `2px`,
     cursor: `pointer`,
     backgroundColor: `white`,
@@ -97,11 +90,12 @@ const SendMessage = ({ scroll, room }) => {
         uploadImage(files[0]);
       }
     };
-
-    fileInput.current.addEventListener('change', handleFileInputChange);
+  
+    const inputRef = fileInput.current;
+    inputRef.addEventListener('change', handleFileInputChange);
     return () => {
-      if (fileInput.current) {
-        fileInput.current.removeEventListener('change', handleFileInputChange);
+      if (inputRef) {
+        inputRef.removeEventListener('change', handleFileInputChange);
       }
     };
   }, [fileInput, uploadImage]);
