787ef60e0b2c7d52b75aaeae3e6ec6e06dbddefb
Hamzah Mansour <hamzahbmansour@gmail.com>
2023-03-27 15:25:33 -0700

    add start of supporting dark mode

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
-import ChatBox from "./components/ChatBox";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import ChatBox from './components/ChatBox';
+
+window.darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
   },
   {
-    path: "/r/:roomName",
+    path: '/r/:roomName',
     element: <ChatBox />,
   },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <RouterProvider router={router} />
